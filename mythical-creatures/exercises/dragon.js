6dfd346c32b71b70bde8d5a22a2eb138d51bd978
Robert Aiken <robertsiraaiken@gmail.com>
2022-10-17 17:54:54 -0500

    Complete dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,27 @@
 
+var count = 0 
+
+class Dragon {
+    constructor (name,rider){
+        this.name = name,
+        this.rider = rider
+        this.hungry = true
+    }
+    greet(){
+        return `Hi, ${this.rider}!`
+    }
+    eat(){
+    count++
+        if (count >= 3){
+            this.hungry = false 
+        }
+    }
+}
+var smaug = new Dragon('smaug');
+console.log(smaug)
+
+
+
+
+
+module.exports = Dragon
